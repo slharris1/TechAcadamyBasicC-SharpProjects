23a0f755581a8aa70afb190ef5f73962230b98b8
slharris1 <101232137+slharris1@users.noreply.github.com>
2022-06-21 21:34:06 -0400

    Branching Assignment
    This code asks the user to input the values of the package weight, height, length and width. If the use weight is larger than 50 pounds the program issues a statement to the user and terminates. If it is under, the program will gather the other dimensions of the package and compute the cost of shipping.

---
+++ BranchingAssignment/BranchingAssignment.cs/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BranchingAssignment.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            Console.ReadLine();
+
+            Console.WriteLine("Please enter the package weight.");
+            string userWeight = Console.ReadLine();
+            int packageWeight = Convert.ToInt32(userWeight);
+            if (packageWeight > 50)
+            {
+                Console.WriteLine("Package is too heavy to be shipped via Package Express. Have a good day!");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+
+            Console.WriteLine("Please enter the package width");
+            string userWidth = Console.ReadLine();
+            int packageWidth = Convert.ToInt32(userWidth);
+
+            Console.WriteLine("Please enter the package height");
+            string userHeight = Console.ReadLine();
+            int packageHeight = Convert.ToInt32(userHeight);
+
+            Console.WriteLine("Please enter the package length");
+            string userLength = Console.ReadLine();
+            int packageLength = Convert.ToInt32(userLength);
+
+            //math
+            int packageDimensions = packageHeight * packageLength * packageWidth;
+            int package = packageDimensions * packageWeight;
+            int packageQuote = package / 100;
+                        
+            Console.WriteLine("Your total is : $" + packageQuote);
+            Console.ReadLine();
+            
+        }
+    }
+}
