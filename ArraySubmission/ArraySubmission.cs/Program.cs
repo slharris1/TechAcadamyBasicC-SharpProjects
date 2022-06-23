c4e3629c62369a878d5204c507f00d16fc4e789b
slharris1 <101232137+slharris1@users.noreply.github.com>
2022-06-23 18:04:14 -0400

    Console App Array Submission
    Create a one-dimensional Array of strings. Ask the user to select an index of the Array and then display the string at that index on the screen.
    
    Create a one-dimensional Array of integers. Ask the user to select an index of the Array and then display the integer at that index on the screen.
    
    Add in a message that displays when the user selects an index that doesn’t exist.
    
    Create a list of strings. Ask the user to select an index of the list and then display the content at that index on the screen.
    
    Add comments to each line or block of your code to explain what it does exactly, so that another developer could read and understand your code.

---
+++ ArraySubmission/ArraySubmission.cs/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraySubmission.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a one-dimensionial Array of strings. Ask the user to select an index of the Array and then display
+            //the integer at that index on the screen. 
+            string[] stringArray = { "apple", "banana", "grape", "orange" };
+            Console.WriteLine("Please select an index of the String Array");
+            int userString= Convert.ToInt32(Console.ReadLine());
+
+            if (userString <4)
+            {
+                Console.WriteLine(stringArray[userString]);
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("That index does not exist. Please try again.");
+                Console.ReadLine();
+            }
+
+            //Create a one-dimensionial Array of integers. Ask the user to select an index of the Array and then display
+            //the integer at that index on the screen. 
+            int[] intArray = { 2, 3, 9, 78, 600 };
+            Console.WriteLine("Please select an index of the Integer Array.");
+            int userInt = Convert.ToInt32(Console.ReadLine());
+            if (userInt < 5)
+            {
+                Console.WriteLine(intArray[userInt]);
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("That index does not exist. Please try again.");
+                Console.ReadLine();
+            }
+
+            //Create a list of strings. Ask the user to select an index of the list and then display the content at that 
+            //index on the screen. 
+            List<string> stringList = new List<string>();
+            stringList.Add("John");
+            stringList.Add("Cindy");
+            stringList.Add("Taylor");
+            stringList.Add("Amy");
+            stringList.Add("Steve");
+            stringList.Add("Louis");
+            Console.WriteLine("Please select an index of the String List.");
+            int indexList = Convert.ToInt32(Console.ReadLine());
+            if (indexList < 6)
+            {
+                Console.WriteLine(stringList[indexList]);
+                Console.ReadLine();
+            }
+            else 
+            {
+                Console.WriteLine("That index does not exist. Please try again.");
+                Console.ReadLine();
+            }
+
+
+
+        }
+    }
+}
