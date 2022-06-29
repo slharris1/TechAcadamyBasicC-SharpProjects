776df1a17a25da46b75e198d1a5539fcd30831d2
slharris1 <101232137+slharris1@users.noreply.github.com>
2022-06-29 18:25:23 -0400

    Example of creating and using methods

---
+++ MethodSubmission/MethodSubmission.cs/ClassOne.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmission.cs
+{
+    public class ClassOne
+    {
+        public int Addition ( int num1 )
+        {
+            int sum = num1 + 5;
+            return sum;
+        }
+
+        public int Multiply (int num1)
+        {
+            int sum = num1 * 3;
+            return sum;
+        }
+
+        public int Division (int num1)
+        {
+            int sum = num1 / 2;
+            return sum;
+        }
+    }
+}
---
+++ MethodSubmission/MethodSubmission.cs/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmission.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number of your choosing.");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+            ClassOne math = new ClassOne(); // creating an instance of the class.
+
+            //This code takes the obj math and passes through the userNumber to the methods.
+            Console.WriteLine(math.Addition(userNumber));
+            Console.WriteLine(math.Multiply(userNumber));
+            Console.WriteLine(math.Division(userNumber));
+            Console.ReadLine();
+
+
+            
+        }
+    }
+}
