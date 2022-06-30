c6ae3bd6c089f3749c6b0b0bbe4381ca22ca7344
slharris1 <101232137+slharris1@users.noreply.github.com>
2022-06-30 17:09:23 -0400

    Method Submission using a default value

--- MethodSubmission2/MethodSubmission2.cs/Program.cs
+++ MethodSubmission2/MethodSubmission2.cs/Program.cs
@@ -14,10 +14,18 @@ namespace MethodSubmission2.cs
 
             Console.WriteLine("Please enter two numbers one at a time. A second number is not needed.");
             int userNumber = Convert.ToInt32(Console.ReadLine());
-            int userNumber2 = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine(math.ExampleOne(userNumber, userNumber2));
-            Console.ReadLine();
+            try
+            {
+                int userNumber2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(math.ExampleOne(userNumber, userNumber2));
+                Console.ReadLine();
+            }
+            catch //passing the catch statement like an else statement. 
+            {
+                Console.WriteLine(math.ExampleOne(userNumber)); //this will use the user input for userNumber and the default value from the method for userNumber2
+                Console.ReadLine();
+            }
         }
     }
 }
