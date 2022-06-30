8da62cfbd3c3077ee22f0c8736863ef83c6ac35a
slharris1 <101232137+slharris1@users.noreply.github.com>
2022-06-29 21:59:38 -0400

    Method Submission 2

---
+++ MethodSubmission2/MethodSubmission2.cs/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmission2.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassOne math = new ClassOne();
+
+            Console.WriteLine("Please enter two numbers one at a time. A second number is not needed.");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+            int userNumber2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(math.ExampleOne(userNumber, userNumber2));
+            Console.ReadLine();
+        }
+    }
+}
