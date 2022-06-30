af394416ee0f23cd66ecfb3eacb732396cdc31af
slharris1 <101232137+slharris1@users.noreply.github.com>
2022-06-29 20:11:16 -0400

    Main Method Assignment

---
+++ MainMethodAssignment/MainMethodAssignment.cs/ClassOne.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment.cs
+{
+    public class ClassOne
+    {
+        //Step One: Create a method that will take in an integer, create a math operation for this integer, then return the answer as an integer.
+        public int Addition(int num1)
+        {
+            int sum = num1 + 5;
+            return sum;
+        }
+
+        //Step Two: Add a method to the class that will take in a decimal, create a different math operation for it then return the answer as an integer. 
+        public int ExampleTwo (int num2)
+        {             
+            int sum = (int)(num2 - 6.0);
+            int sum1= (int)Convert.ToDecimal(sum);
+            return sum1;
+        }
+         
+
+        //Step Three: Add a method that will take in a string, convert it to an integer (if possible), do a different math operationto it then return the answer as an integer. 
+        public int ExampleThree (int num3)
+        {
+            int newOperation = Convert.ToInt32(num3);
+            int sum = newOperation + 369;
+            return sum;
+        }
+    }
+}
---
+++ MainMethodAssignment/MainMethodAssignment.cs/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Step One:
+            Console.WriteLine("Please enter a number of your choosing.");
+            int userInput1 = Convert.ToInt32(Console.ReadLine());
+            ClassOne math = new ClassOne();
+            Console.WriteLine(math.Addition(userInput1));
+            Console.ReadLine();
+
+            //Step Two:
+            Console.WriteLine("Please enter a number of your choosing.");
+            int stepTwo = (int)Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine(math.ExampleTwo(stepTwo));
+            Console.ReadLine();
+
+            //Step Three:
+            Console.WriteLine("Please enter a number of your choosing.");
+            string userInput = Console.ReadLine();
+            int stepThree = Convert.ToInt32(userInput);
+            Console.WriteLine(math.ExampleThree(stepThree));
+            Console.ReadLine();
+        }
+
+    }
+}
