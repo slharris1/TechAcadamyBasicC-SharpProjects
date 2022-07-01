b57e58006866bfb20559494788f87d2c5f85bfc9
slharris1 <101232137+slharris1@users.noreply.github.com>
2022-06-30 22:17:04 -0400

    Methods and Objects

---
+++ MethodsAndObjects/MethodsAndObjects.cs/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjects.cs
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+
+
+        public void SayName()
+        {
+            Console.WriteLine("Name: {0}", FirstName +" "+ LastName);           
+        }
+    }
+}
