ac59e457dc811377b47e4231487ce532a2e30351
slharris1 <101232137+slharris1@users.noreply.github.com>
2022-07-01 19:10:25 -0400

    Abstract classes

---
+++ AbstractClasses/AbstractClasses.cs/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClasses.cs
+{
+    public abstract class Person
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        public virtual void SayName()
+        {
+            Console.WriteLine("Name: {0}", firstName + " " + lastName);
+        }
+    }
+
+
+}
