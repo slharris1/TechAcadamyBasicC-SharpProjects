62dabfd332424d8d1a6adfe08fa0b96273bb1eb3
slharris1 <101232137+slharris1@users.noreply.github.com>
2022-06-30 20:26:39 -0400

    Class Submission Assignment

---
+++ ClassMethods/ClassMethods.cs/ClassOne.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Exercise that creates methods that specifically belong to a class and more on how to access those methods without creating an object of the class first. 
+namespace ClassMethods.cs
+{
+    public class ClassOne
+    {
+        //Example One:
+        public void Example1 (int num1)
+        {
+            int sum = num1 / 2;
+            Console.WriteLine(sum);
+        }
+
+        //Example Two:
+        public void Value(out int num1)
+        {
+            int randomeNumber = 369;
+            num1 = randomeNumber;
+            Console.WriteLine(num1);
+        }
+
+        //Example Three: 
+        public int Multiply (int a, int b)
+        {
+            int sum1 = a * b;
+            return sum1;
+        }
+        
+    }
+
+
+    static class Static
+    {
+        public static string Example2()
+        {
+            string phrase1 = "That is all folks";
+            return phrase1;
+        }
+    }
+    
+}
+
+//objects have state (properties) and behavior (they can do things)
+
+
---
+++ ClassMethods/ClassMethods.cs/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethods.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Example One:
+            ClassOne math = new ClassOne(); // creating an instance of ClassOne                     
+            Console.WriteLine("Please enter a number of your choosing.");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+
+            math.Example1(userNumber);
+            Console.ReadLine();
+
+
+            //Example Two: 
+            ClassOne value = new ClassOne();
+            int z = 1000;
+            value.Value(out z);
+            Console.ReadLine();
+
+            //Example Three: 
+            ClassOne overload = new ClassOne();
+            int sum1 = overload.Multiply(10, 5);
+            Console.WriteLine(sum1);
+            Console.ReadLine();
+
+            //Example Four:
+            Console.WriteLine(Static.Example2());
+            Console.ReadLine();
+        }
+    }
+}
