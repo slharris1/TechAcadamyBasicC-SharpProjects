01af63614034a9198ef36384543f50e28ee6df06
slharris1 <101232137+slharris1@users.noreply.github.com>
2022-07-01 22:23:07 -0400

    Overloading Operators Submission

---
+++ OperatorsSubmission.cs/OperatorsSubmission.cs/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsSubmission.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.FirstName = "Sam";
+            employee.LastName = "Test";
+            employee.Id = 2;
+
+            Employee employee2 = new Employee() { FirstName = "Tim", LastName = "Jones", Id = 8 };
+
+            Console.WriteLine(employee == employee2);
+            Console.ReadLine();
+            
+        }
+    }
+}
