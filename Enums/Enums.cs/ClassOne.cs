c1cda776a4f9248efc4b0cfbc3e7706856e7295a
slharris1 <101232137+slharris1@users.noreply.github.com>
2022-07-02 19:32:58 -0400

    ENUMS

---
+++ Enums/Enums.cs/ClassOne.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums.cs
+{
+    class ClassOne
+    {
+    }
+
+    public enum DaysOfTheWeek
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+}
---
+++ Enums/Enums.cs/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums.cs
+
+{
+    public enum DaysOfTheWeek
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the current day of the week");
+            string userDay = Console.ReadLine();
+            DaysOfTheWeek day;
+
+            try
+            {
+                bool checkParse = Enum.TryParse(userDay, out day);
+                Console.WriteLine(checkParse);
+                Console.ReadLine();
+            }
+            catch
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+                Console.ReadLine();
+            }
+
+
+        }
+
+    }
+
+    
+}
+
