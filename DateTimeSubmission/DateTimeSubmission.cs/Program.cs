9a9bd1041bbee1b6ad41e78cb9987cfcf15e1add
slharris1 <101232137+slharris1@users.noreply.github.com>
2022-07-04 17:15:53 -0400

    DateTime Submission

---
+++ DateTimeSubmission/DateTimeSubmission.cs/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeSubmission.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dateTime = new DateTime();
+            Console.WriteLine(DateTime.Now);
+            Console.ReadLine();
+
+            Console.WriteLine("Please enter an number");
+            int userResponse = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(dateTime.AddHours(userResponse));
+            Console.ReadLine();
+        }
+    }
+}
