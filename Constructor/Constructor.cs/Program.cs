9eaea8b11d6226304b2d9be48c2cd4cfb20ec5b0
slharris1 <101232137+slharris1@users.noreply.github.com>
2022-07-05 20:01:08 -0400

    Constructor Assignment

---
+++ Constructor/Constructor.cs/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string projectName = "This is a constructor Assignment";
+
+            Console.WriteLine(projectName);
+
+            var age = 27;
+            Console.WriteLine(age + 5);
+
+                  
+        }
+    }
+}
