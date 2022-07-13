219f627bbdc53601cea7d7fb93e0b5ff035ea787
slharris1 <101232137+slharris1@users.noreply.github.com>
2022-07-12 20:53:22 -0400

    Challenge Assignment
    Design a simple web app that displays actual time.

---
+++ WApp/WebApplication2/Pages/Index.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace WebApplication2.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            string dateTime = DateTime.Now.ToShortTimeString();
+            ViewData["TimeStamp"] = dateTime;
+
+        }
+    }
+}
